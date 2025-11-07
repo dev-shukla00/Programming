f059523315a22e1e4b3472bdbf73413c671fbeec
Immortal <shukladev2027@gmail.com>
2025-11-07 18:59:45 +0530

    practice question, print even and odd numbers

---
+++ Web-Security/7_practice.js
@@ -0,0 +1,15 @@
+// Print all even numbers from 0 to 100.
+
+for (let num = 0; num <= 100; num++) {
+    if (num % 2 === 0) {
+        console.log("even: ", num);
+    }
+}
+
+// print all odd numbers from 0 to 100.
+
+for (let odd = 0; odd <= 100; odd++) {
+    if (odd % 2 === 1) {
+        console.log("odd:", odd);
+    }
+}
