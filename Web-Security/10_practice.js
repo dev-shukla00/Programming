eee6e9f90565c39282ce1a87de2eb4f9b484cca0
Dev Shukla <shukladev2027@gmail.com>
2025-11-26 14:34:44 +0530

    completed arrays

--- Web-Security/10_practice.js
+++ Web-Security/10_practice.js
@@ -1,1 +1,61 @@
-//Arrays;
+//Arrays; Collections of items :
+
+let bounties = ["500, 1000, 1,500, 2,000"];
+    
+//Accessing Array Items :
+let firstBounty = bounties[0]; // "500"
+let secondBounty = bounties[1]; // "1000"
+
+//Modifying Array Items :
+bounties[2] = "1,200"; // Changes the third item to "1,200"
+
+//Array Properties and Methods :
+let numberOfBounties = bounties.length; // 4
+
+//Adding Items to an Array :
+bounties.push("2,500"); // Adds "2,500" to the end of the array
+
+//Removing Items from an Array :
+let lastBounty = bounties.pop(); // Removes and returns the last item ("2,500")
+
+//Looping Through an Array :
+for (let index = 0; index < bounties.length; index++) {
+    console.log(bounties[index]);
+}
+
+//Using forEach method :
+bounties.forEach(function(bounty) {
+    console.log(bounty);
+});
+
+//Multidimensional Arrays :
+let bountyMatrix = [
+    ["500", "1000"],
+    ["1500", "2000"]
+];
+
+//Accessing items in a multidimensional array :
+let firstRowFirstBounty = bountyMatrix[0][0]; // "500"
+let secondRowSecondBounty = bountyMatrix[1][1]; // "2000"
+
+//Adding a new row to the multidimensional array :
+bountyMatrix.push(["2500", "3000"]);
+
+//Removing the last row from the multidimensional array :
+let lastRow = bountyMatrix.pop(); // Removes and returns the last row (["2500", "3000"])
+
+//Looping through a multidimensional array :
+for (let i = 0; i < bountyMatrix.length; i++) {
+    for (let j = 0; j < bountyMatrix[i].length; j++) {
+        console.log(bountyMatrix[i][j]);
+    }
+}
+
+//Using forEach method for multidimensional array :
+bountyMatrix.forEach(function(row) {
+    row.forEach(function(bounty) {
+        console.log(bounty);
+    });
+});         
+
+//End of Arrays Practice Code
